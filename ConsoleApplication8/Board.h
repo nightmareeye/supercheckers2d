b4fa58ed6622c347a22e8fd995d2d4d39584524e
xSkywa1ker <you@example.com>
2021-12-13 07:22:26 +0300

    Made method for getting squares and field for cells

--- ConsoleApplication8/Board.h
+++ ConsoleApplication8/Board.h
@@ -5,5 +5,10 @@ using namespace sf;
 using namespace std; 
 
 class Board {
-
+private:
+public:
+	Square& get_all_squares(int i, int j) { // получение €чейки с номером i, j
+		return this->all_squares[i][j];
+	}
+	Square all_squares[8][8]; //количество €чеек
 };
