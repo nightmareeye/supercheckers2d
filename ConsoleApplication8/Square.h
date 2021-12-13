7a753a3a15a7a0272e8e2e3f1f4a1d1144a1a798
xSkywa1ker <you@example.com>
2021-12-13 06:58:08 +0300

    Added method for free cells and fields for it. Also made method for different colors of cell

--- ConsoleApplication8/Square.h
+++ ConsoleApplication8/Square.h
@@ -10,6 +10,14 @@ public:
 	bool get_fon_active() { // передает есть ли подцветка
 		return this->fon_active;
 	}
+	bool get_free() { // свободна ли клетку
+		return this->free;
+	}
+	bool get_checker_color() { // передает цвет клетки
+		return this->checker_color;
+	}
 private:
 	bool fon_active = 0; // 1 подцветки есть, 0 = нет подцветки 
+	bool free = 0; // 1 = заняте клетки, 0 = свободные клетки
+	bool checker_color; // 1 = черные шашки, 0 = белые шашки
 };
