ca187cd56006826e19545d3642145eb111fa9c12
xSkywa1ker <you@example.com>
2021-12-13 06:59:13 +0300

    Added methods for turning on and off the backlight

--- ConsoleApplication8/Square.h
+++ ConsoleApplication8/Square.h
@@ -16,8 +16,15 @@ public:
 	bool get_checker_color() { // передает цвет клетки
 		return this->checker_color;
 	}
+	void off_fon_active() { // убрать подцветку
+		this->fon_active = 0;
+	}
+	void on_fon_active() { // добавить подцветку
+		this->fon_active = 1;
+	}
 private:
 	bool fon_active = 0; // 1 подцветки есть, 0 = нет подцветки 
 	bool free = 0; // 1 = заняте клетки, 0 = свободные клетки
 	bool checker_color; // 1 = черные шашки, 0 = белые шашки
+	RectangleShape cell = RectangleShape(Vector2f(80, 80)); // клетка размером 80х80
 };
