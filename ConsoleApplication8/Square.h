fa4674424a12fdbe5b6f91b7c447f45c09d867d6
xSkywa1ker <you@example.com>
2021-12-13 06:56:13 +0300

    Added method for cells and backlight, also added field for backlight

--- ConsoleApplication8/Square.h
+++ ConsoleApplication8/Square.h
@@ -2,10 +2,14 @@
 using namespace sf;
 
 class Square {
+public:
+
+	RectangleShape& get_cell() { // передает клетку
+		return this->cell;
+	}
+	bool get_fon_active() { // передает есть ли подцветка
+		return this->fon_active;
+	}
 private:
-	bool fon_active = 0; // 1 подцветки есть, 0 = нет подцветки  
-	bool free = 0; // 1 = заняте клетки, 0 = свободные клетки
-	bool checker_color; // 1 = черные шашки, 0 = белые шашки
-	RectangleShape cell = RectangleShape(Vector2f(80, 80)); // клетка размером 80х80
-};
+	bool fon_active = 0; // 1 подцветки есть, 0 = нет подцветки 
 };
