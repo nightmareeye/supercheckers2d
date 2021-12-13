aab1b0fcd853cc8d65b4f5712864e20a63667e2e
nightmareeye <mrrexcrafter@gmail.com>
2021-12-13 07:25:16 +0300

    Added public methods, checkers draw & delete_blacklight

--- ConsoleApplication8/Checkers_on_board.h
+++ ConsoleApplication8/Checkers_on_board.h
@@ -6,6 +6,38 @@ using namespace sf;
 
 class Checkers_on_board {
 public:
+	Board& get_board() {
+		return this->board;
+	}
+
+	Checker& get_checker(int i) { 
+		return this->checkers[i];
+	}
+
+	int get_size() { 
+		return this->checkers.size();
+	}
+	void delete_checker() { 
+		checkers.pop_back();
+	}
+
+	Checkers_on_board() {
+		start_game(); 
+	};
+
+	void draw_checkers(RenderWindow& window) { 
+		board.draw_board(window); 
+		for (int i = 0; i < checkers.size(); i++) {
+			checkers[i].draw_checker(window); 
+		}
+	}
+	void delete_backlight() { 
+		for (int i = 0; i < 8; i++) {
+			for (int j = 0; j < 8; j++) {
+				this->board.get_all_squares(i, j).off_fon_active();
+			}
+		}
+	}
 private:
 	vector <Checker> checkers;
 	Board board;
