dd20669b6778fe5ac208d32aef81c19e94ccde5b
xSkywa1ker <you@example.com>
2021-12-13 07:39:09 +0300

    Added fields for change color, seletion, changing number, coordinates, position of mouse, eating checkers of class Game

--- ConsoleApplication8/Game.h
+++ ConsoleApplication8/Game.h
@@ -5,4 +5,12 @@ using namespace sf;
 class Game {
 public:
 private:
+	bool move_color = 0; 
+	bool selection = 0; 
+	int checker_num; 
+	float x; 
+	float y; 
+	Vector2i pos_of_mouse; 
+	Checkers_on_board checkers_on_board;
+	vector <int> eat_checkers; \
 };
