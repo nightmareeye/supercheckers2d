a0818c0367966062282d6148c71c43d2a74bb0f3
nightmareeye <mrrexcrafter@gmail.com>
2021-12-13 08:14:26 +0300

    Added .cpp

--- ConsoleApplication8/shashki.cpp
+++ ConsoleApplication8/shashki.cpp
@@ -1,5 +1,23 @@
-﻿#include <iostream>
+﻿#include <SFML/Graphics.hpp>
+#include "Game.h"
+using namespace sf;
+#pragma comment(linker, "/SUBSYSTEM:windows /ENTRY:mainCRTStartup")
 
-int main()
-{
+int main() {
+	setlocale(LC_ALL, "rus"); // установка русского языка
+	Game game; // игра
+	bool start = 1;
+	RenderWindow window(VideoMode(800, 800), "Checker"); // окно программы
+	while (window.isOpen()) {
+		Event event;
+		while (window.pollEvent(event)) {
+			if (event.type == Event::Closed)
+				window.close();
+			game.make_move(window, event);
+		}
+		window.clear(Color(236, 200, 159));
+		game.get_checkers_on_board().draw_checkers(window); // рисую поле и шашки
+		window.display();
+	}
+	return 0;
 }
